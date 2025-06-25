426e7cfb337ecf6320530558a989700ea4bc694b
zahraa <ftoomrrt@gmail.com>
2025-06-25 03:28:34 +0300

    .

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify , current_app
+from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from . import db
 import os
@@ -9,12 +9,11 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 from email.utils import formataddr
-from flask_mail import Message
-from . import mail
-
 load_dotenv()
 main = Blueprint('main', __name__)
 
+# authorization user 
+# 1- register 
 @main.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
@@ -32,7 +31,7 @@ def register():
 
     return jsonify({'message': 'User registered successfully'}), 201
 
-
+# 2-login
 @main.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -42,7 +41,7 @@ def login():
         return jsonify({'token': token}), 200
     return jsonify({'error': 'Invalid credentials'}), 401
 
-
+# 3- create the order for the user 
 @main.route("/order", methods=["POST"])
 @jwt_required()
 def create_order():
@@ -72,28 +71,43 @@ def create_order():
 
     return jsonify({"message": "Order placed successfully", "order_id": order.id})
 
-
+# 4- send confirm email for the authorized user
 def send_confirmation_email(order, product):
+    sender_email = os.getenv("SENDER_EMAIL")
+    sender_password = os.getenv("SENDER_PASSWORD")
+    receiver_email = order.customer_email
+
+    # check the presences of email  data 
+    if not sender_email or not sender_password:
+        return jsonify({"error": "Email credentials are missing"}), 500
+
+    #Words in email 
+    subject = f"Order Confirmation - Order #{order.id}"
+
+    html = f"""
+    <html>
+        <body>
+            <h2>Thank you for your order!</h2>
+            <p>Order ID: #{order.id}</p>
+            <p>{order.quantity} &times; {product.name} = ${order.total}</p>
+        </body>
+    </html>
+    """
+
+    #Setup email
+    message = MIMEMultipart()
+    message["From"] = formataddr((str(Header("Order Service", "utf-8")), sender_email))
+    message["To"] = receiver_email
+    message["Subject"] = Header(subject, "utf-8")
+    message.attach(MIMEText(html, "html", "utf-8"))
+
+    #Send email
     try:
-        # subject = f"Order Confirmation - Order #{order.id}"
-
-        body = f"""
-        Thank you for your order!
-        Order ID: {order.id}
-        Product: {product.name}
-        Quantity: {order.quantity}
-        Total: ${order.total}
-        """
-
-        msg = Message(
-            # subject=subject,
-            sender=current_app.config['MAIL_USERNAME'],
-            recipients=[order.customer_email],
-            body=body
-        )
-
-        mail.send(msg)
-        print("Email sent successfully!")
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+            print("Email sent successfully!")
     except Exception as e:
-        print(f"Email failed: {e}")
+        print(f" Email sending failed: {e}")
         return jsonify({"error": "Email failed", "details": str(e)}), 500
